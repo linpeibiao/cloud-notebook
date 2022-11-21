378a1385eeff589823da62774193b18d0f5af4c9
沛彪 林 <linpeibiao2022@163.com>
2022-11-21 16:31:02 +0800

    feat: 添加登录拦截器

---
+++ common/src/main/java/com/xiaohu/cloud_notebook_common/config/MvcConfig.java
@@ -0,0 +1,8 @@
+package com.xiaohu.cloud_notebook_common.config;
+/**
+ * @author xiaohu
+ * @date 2022/11/19/ 16:59
+ * @description
+ */
+public class MvcConfig {
+}
---
+++ common/src/main/java/com/xiaohu/cloud_notebook_common/intercepter/LoginInterceptor.java
@@ -0,0 +1,10 @@
+package com.xiaohu.cloud_notebook_common.intercepter;
+
+/**
+ * @author xiaohu
+ * @date 2022/11/19/ 16:55
+ * @description
+ */
+public class LoginIntecepter {
+
+}
